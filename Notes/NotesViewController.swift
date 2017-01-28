5fc38c4dac590bb6dfcc2fa2b7d33f490ee78e1b
Tarek Morshed <tarekmorshed@Tarek-2.local>
2017-01-28 21:41:07 +0200

    Done :)

--- Notes/NotesViewController.swift
+++ Notes/NotesViewController.swift
@@ -1,20 +1,58 @@
-
-
 import UIKit
 
 class NotesViewController: UIViewController {
-
+    
+    @IBOutlet weak var deleteBTN: UIBarButtonItem!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteTextArea: UITextView!
+    
+    var _note : NoteCls? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-            }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        if _note != nil{
+            titleTextField.text = _note?.title
+            noteTextArea.text = _note?.note
+        }
+        
     }
     
-
     
+    @IBAction func saveAction(_ sender: Any) {
+        
+        if _note != nil{
+            _note?.title = titleTextField.text
+            _note?.note  = noteTextArea.text
+        }
+        else{
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            
+            let note = NoteCls(context: context)
+            
+            note.title = titleTextField?.text
+            note.note = noteTextArea?.text
 
+        }
+        
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+    }
+    
+    @IBAction func deleteAction(_ sender: Any) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(_note!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
+
+        
+        
+    }
+    
+    
 }
--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -1,25 +1,63 @@
-//
-//  ViewController.swift
-//  Notes
-//
-//  Created by Tarek Morshed on 1/28/17.
-//  Copyright © 2017 Tarek Morshed. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var noteList : [NoteCls] = []
+    
     override func viewDidLoad() {
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do{
+            noteList = try context.fetch(NoteCls.fetchRequest())
+            tableView.reloadData()
+        }catch{
+            
+        }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return noteList.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text  = noteList[indexPath.row].title
+        cell.imageView?.image = UIImage(named: "n1")
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let note = noteList[indexPath.row]
+        performSegue(withIdentifier: "noteSegue", sender: note)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+        let nextVC = segue.destination as! NotesViewController
+        nextVC._note = sender as? NoteCls
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
 
 }
 
